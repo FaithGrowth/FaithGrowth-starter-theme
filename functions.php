ccd29c8e81e9a948f18ce24f528a9f66a573385f
Christopher Harris <christopher@faithgrowth.com>
2014-08-06 06:42:33 -0500

    Remove support for custom background

--- functions.php
+++ functions.php
@@ -21,8 +21,5 @@ add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list'
 //* Add viewport meta tag for mobile browsers
 add_theme_support( 'genesis-responsive-viewport' );
 
-//* Add support for custom background
-add_theme_support( 'custom-background' );
-
 //* Add support for 3-column footer widgets
 add_theme_support( 'genesis-footer-widgets', 3 );
